d032a57b62b8c91cb85b8930f817ae467b446aae
Ivette Hernández <3238938+IvetteLovegood@users.noreply.github.com>
2023-06-20 20:24:50 -0600

    fix

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -7,14 +7,14 @@ const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(true);
 
   return (
-    <div className={`h-screen ${isOpen ? 'w-64' : 'w-24'} bg-gray-200 p-4 transition-width duration-200`}>
+    <div className={`h-screen ${isOpen ? 'w-64' : 'w-24'} bg-blue-500 p-4 transition-width duration-200`}>
       <button onClick={() => setIsOpen(!isOpen)} className="mb-4 bg-blue-500 text-white p-2 rounded">
         {isOpen ? 'Collapse' : 'Expand'}
       </button>
       <nav>
         <ul className="space-y-2">
-          <li><Link href="/user">Users</Link></li>
-          <li><Link href="/contact">Contacts</Link></li>
+          <li className='text-white'><Link href="/user">Users</Link></li>
+          <li className='text-white'><Link href="/contact">Contacts</Link></li>
         </ul>
       </nav>
     </div>
